227c7a19d6267dc99abd7f1a1fd15aa69b7cfcb0
Akhtar Ansari <80877589+Punkakz@users.noreply.github.com>
2025-12-10 21:37:52 +0530

    Update deployService.groovy

--- vars/deployService.groovy
+++ vars/deployService.groovy
@@ -1,16 +1,8 @@
-def call(String serverIp, String remotePath) {
-
-    sh """
-        echo 'Deploying to EC2: ${serverIp}'
-
-        # Copy docker-compose file
-        scp -o StrictHostKeyChecking=no deploy/docker-compose.yml ubuntu@${serverIp}:${remotePath}/docker-compose.yml
-
-        # Run Docker Compose on EC2
-        ssh -o StrictHostKeyChecking=no ubuntu@${serverIp} "
-            cd ${remotePath}
-            docker compose pull
-            docker compose up -d --remove-orphans
-        "
-    """
+def call(String serverIp, String deployPath) {
+    sshagent(credentials: ['ec2-ssh']) {
+        sh """
+            ssh -o StrictHostKeyChecking=no ubuntu@${serverIp} \
+            "cd ${deployPath} && docker compose pull && docker compose up -d"
+        """
+    }
 }
