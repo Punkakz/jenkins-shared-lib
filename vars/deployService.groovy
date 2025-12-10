8e5e3153f809e41356f5091e9e3bd7b96db4e889
Akhtar Ansari <80877589+Punkakz@users.noreply.github.com>
2025-12-10 21:32:56 +0530

    Update deployService.groovy

--- vars/deployService.groovy
+++ vars/deployService.groovy
@@ -1,14 +1,16 @@
-// deployService(serverIp, remotePath)
-def call(String serverIp, String remotePath = '/home/ubuntu/deploy/snake-game') {
-    stage('Deploy to EC2') {
-        // Assumes Jenkins has SSH access to ubuntu@serverIp (key in credentials or agent ssh key)
-        sh """
-          scp -o StrictHostKeyChecking=no deploy/docker-compose.yml ubuntu@${serverIp}:${remotePath}/docker-compose.yml
-          ssh -o StrictHostKeyChecking=no ubuntu@${serverIp} '
-             cd ${remotePath} &&
-             docker compose pull &&
-             docker compose up -d --remove-orphans
-          '
-        """
-    }
+def call(String serverIp, String remotePath) {
+
+    sh """
+        echo 'Deploying to EC2: ${serverIp}'
+
+        # Copy docker-compose file
+        scp -o StrictHostKeyChecking=no deploy/docker-compose.yml ubuntu@${serverIp}:${remotePath}/docker-compose.yml
+
+        # Run Docker Compose on EC2
+        ssh -o StrictHostKeyChecking=no ubuntu@${serverIp} "
+            cd ${remotePath}
+            docker compose pull
+            docker compose up -d --remove-orphans
+        "
+    """
 }
