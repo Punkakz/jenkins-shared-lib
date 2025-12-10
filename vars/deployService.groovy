538a73dcfafc9aab180244ca40f52be5a8df6dd0
Akhtar Ansari <80877589+Punkakz@users.noreply.github.com>
2025-12-10 22:04:28 +0530

    Update deployService.groovy

--- vars/deployService.groovy
+++ vars/deployService.groovy
@@ -1,14 +1,22 @@
 def call(String deployPath) {
 
     stage("Deploy Application") {
+
         sh """
-            echo "Deploying locally on the same EC2..."
+            echo "🚀 Deploying locally on Jenkins/EC2..."
 
             cd ${deployPath}
 
+            # Stop old containers (if running)
             docker compose down || true
+
+            # Pull latest image from Docker Hub
             docker compose pull
+
+            # Start updated containers
             docker compose up -d --remove-orphans
+
+            echo "✅ Deployment finished successfully!"
         """
     }
 }
