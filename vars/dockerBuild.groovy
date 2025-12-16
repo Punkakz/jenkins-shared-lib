e311117cbd96c3c6e756832ef0acf7233e340e26
Akhtar Ansari <80877589+Punkakz@users.noreply.github.com>
2025-12-16 11:16:33 +0530

    Refactor dockerBuild function to include tagging

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,11 @@
+//def call(String image, String tag) {
+  //  sh "docker build -t ${image}:${tag} ."
+//}
+
 def call(String image, String tag) {
-    sh "docker build -t ${image}:${tag} ."
+    sh """
+        docker build -t ${image}:${tag} .
+        docker tag ${image}:${tag} ${image}:latest
+    """
 }
+
