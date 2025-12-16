38fcb5b49d33ce053c33fe1966c09f904555a99f
Akhtar Ansari <80877589+Punkakz@users.noreply.github.com>
2025-12-16 09:52:07 +0530

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,9 +1,30 @@
 // Build the Maven project and publish test results
+//def call() {
+  //  stage('Build & Test') {
+    //    sh "mvn -B -DskipTests=false clean package"
+ //   }
+ //   stage('Publish Tests') {
+    //    junit allowEmptyResults: true, testResults: 'target/surefire-reports/*.xml'
+  //  }
+//}
+
 def call() {
     stage('Build & Test') {
-        sh "mvn -B -DskipTests=false clean package"
+        sh "mvn -B clean package"
     }
+
     stage('Publish Tests') {
         junit allowEmptyResults: true, testResults: 'target/surefire-reports/*.xml'
+    }
+
+    stage('Deploy') {
+        sh '''
+        echo "Stopping old Snake app..."
+        pkill -f snake || true
+        pkill -f java || true
+
+        echo "Starting new Snake app..."
+        nohup java -jar target/*.jar > snake.log 2>&1 &
+        '''
     }
 }
